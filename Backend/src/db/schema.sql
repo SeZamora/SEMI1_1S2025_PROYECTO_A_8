363fe3f8d8df48f279445bb952428b2cca912ebe
Jhonatan <3227943820801@ingenieria.usac.edu.gt>
2025-04-27 10:57:52 -0600

    schema

---
+++ Backend/src/db/schema.sql
@@ -0,0 +1,39 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL, -- encriptado (hasheado)
+    profile_picture VARCHAR(255), -- URL o ruta de la imagen
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE IF NOT EXISTS credentials (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    site_name VARCHAR(255) NOT NULL,          -- Nombre del servicio “Gmail”
+    username VARCHAR(255) NOT NULL,           -- Usuario en ese servicio
+    password_encrypted TEXT NOT NULL,         -- Contraseña cifrada / encriptada 
+    note TEXT,                                -- Nota o descripción (este es opcional)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS user_faces (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    rekognition_face_id VARCHAR(255) NOT NULL,  -- El FaceId devuelto por Rekognition
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    INDEX idx_user_faces_user (user_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS files (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(50) NOT NULL, -- MIME Type (ej. image/png, text/plain)
+    path VARCHAR(255) NOT NULL, -- Ruta donde se almacena el archivo
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
